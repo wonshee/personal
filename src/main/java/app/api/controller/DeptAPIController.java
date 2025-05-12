d232a98b5763cfdd480a04a5c217de729c20e436
wonshee <shee2.won@gmail.com>
2025-05-12 14:04:45 +0900

    add feature : gitApi

--- src/main/java/app/api/controller/DeptAPIController.java
+++ src/main/java/app/api/controller/DeptAPIController.java
@@ -22,6 +22,10 @@ public class DeptAPIController {
 	
 	private final DeptService deptService;
 	
+	@GetMapping("/git")
+	public String gitApi() {
+		return "git controller";
+	}
 	
 	@GetMapping("/test")
 	public String deptApi() {
